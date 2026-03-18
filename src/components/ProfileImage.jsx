b9349e9c41287b4fc6702460c709137f73ba9071
ryan-errando <errandoryan@gmail.com>
2026-03-18 16:29:34 +0700

    Remove deleted blue circle asset import and node

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './ProfileImage.css';
-import blueCircle from '../assets/blue_circle.png';
 
 const ProfileImage = () => {
   return (
     <div className="profile-image-container">
-      {/* Outer static ring matching the image perfectly */}
-      <img src={blueCircle} alt="Circle Border" className="circular-dash-ring" />
       
       <div className="image-wrapper">
         <img 
